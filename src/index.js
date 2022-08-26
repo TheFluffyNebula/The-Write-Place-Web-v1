63cf200b54956199cee919bc5cf642a38fdc462b
TheFluffyNebula <yinj@guilford.edu>
2022-08-25 23:07:26 -0400

    initialized auth, firestore, and storage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import {initializeApp} from 'firebase/app';
-
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+import {getStorage} from 'firebase/storage';
 const firebaseApp = initializeApp({
-    
+    apiKey: "AIzaSyCQ1As5zCwlIDx_iU3S2-zK8Fy-O-DvVVc",
+    authDomain: "the-write-place-ea1e8.firebaseapp.com",
+    projectId: "the-write-place-ea1e8",
+    storageBucket: "the-write-place-ea1e8.appspot.com",
+    messagingSenderId: "144537031501",
+    appId: "1:144537031501:web:381f1b2964a4e95c049d04",
+    measurementId: "G-RFS3FW3HTE"
 })
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
